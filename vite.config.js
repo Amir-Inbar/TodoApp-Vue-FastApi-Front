bec5bb935df4d41e657dd427871e698342bf8e7a
amirinbar <amir.inbar@lynx.md>
2022-10-08 10:23:25 +0300

    uploading to github

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    outDir: '../Backend/public',
+    emptyOutDir: true,
+  },
 })
