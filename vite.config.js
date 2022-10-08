992da87c714ecddd398eaa152ddda5d0071e6aa7
amirinbar <amir.inbar@lynx.md>
2022-10-08 10:59:17 +0300

    upoading

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-
-  build: {
-    outDir: '../Backend/public',
-    emptyOutDir: true,
-  },
 })
